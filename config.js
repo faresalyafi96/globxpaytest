229a285e7479cf83173c70f39751c13880ad67fd
faresalyafi96 <faresalyafi966@gmail.com>
2026-02-14 18:25:43 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,20 @@
+const config = {
+    files: [
+        {
+            name: 'تنزيل ملف ترخيص الشركة ',
+            file: 'globxpaypres.pdf'
+        },
+               {
+            name: 'تنزيل ملف نموذج تغير الرقم',
+            file: 'Change number form.pdf'
+        },
+               {
+            name: 'تنزيل ملف Charge Back',
+            file: 'Chargeback form.pdf'
+        },
+        {
+            name: 'تنزيل ملف Globx Agent Networks',
+            file: 'GlobXpay Networks.xlsx'
+        }
+    ]
+};
